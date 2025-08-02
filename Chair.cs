9b6e86648921d43eb641c7f1029acb6722d4263f
Ccode-lang <coop112097@gmail.com>
2025-08-02 10:32:49 -0700

    Add project files.

---
+++ Chair.cs
@@ -0,0 +1,130 @@
+﻿using GameNetcodeStuff;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace TheRollingChair
+{
+    internal class Chair : EnemyAI
+    {
+
+        PlayerControllerB selectedPlayer = null;
+
+        public GameObject RopePhysics;
+        public GameObject Noose;
+
+        public Transform ViewPoint;
+
+        public override void DoAIInterval()
+        {
+            base.DoAIInterval();
+            if (StartOfRound.Instance.allPlayersDead)
+            {
+                return;
+            }
+            if (isEnemyDead)
+            {
+                return;
+            }
+            if (selectedPlayer == null)
+            {
+                return;
+            }
+
+            if (!selectedPlayer.isInsideFactory)
+            {
+                agent.speed = 0;
+                agent.angularSpeed = 0;
+            }
+
+            if (!selectedPlayer.HasLineOfSightToPosition(ViewPoint.position))
+            {
+                // Do not touch the magic 1.8f
+                agent.acceleration = Plugin.RollSpeed.Value * 1.8f;
+                agent.speed = Plugin.RollSpeed.Value;
+                agent.angularSpeed = 120;
+                SetDestinationToPosition(RoundManager.Instance.GetNavMeshPosition(selectedPlayer.transform.position, RoundManager.Instance.navHit, 2.75f));
+            }
+            else
+            {
+                agent.acceleration = 0.2f;
+                agent.speed = 0;
+                agent.angularSpeed = 0;
+                SetDestinationToPosition(transform.position);
+            }
+        }
+
+        private PlayerControllerB nearestPlayer()
+        {
+            float distance = float.MaxValue;
+            PlayerControllerB nearPlayer = null;
+            foreach (PlayerControllerB player in StartOfRound.Instance.allPlayerScripts)
+            {
+                float distanceTemp = Vector3.Distance(player.transform.position, transform.position);
+                if (distanceTemp < distance)
+                {
+                    distance = distanceTemp;
+                    nearPlayer = player;
+                }
+            }
+
+            return nearPlayer;
+        }
+
+        public override void OnCollideWithPlayer(Collider other)
+        {
+            base.OnCollideWithPlayer(other);
+            if (!selectedPlayer.HasLineOfSightToPosition(ViewPoint.position)) {
+                other.gameObject.GetComponent<PlayerControllerB>().DamagePlayer(100, false, true, CauseOfDeath.Strangulation, 0, false, default);
+                HangPlayerServerRpc(new NetworkObjectReference(other.gameObject.GetComponent<NetworkObject>()));
+            }
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void HangPlayerServerRpc(NetworkObjectReference player) {
+            HangPlayerClientRpc(player);
+        }
+
+        [ClientRpc]
+        public void HangPlayerClientRpc(NetworkObjectReference player)
+        {
+            // Most of this is pretty much just sand spider code since it works well
+            NetworkObject playerClient;
+            if (player.TryGet(out playerClient, null))
+            {
+                PlayerControllerB playerController = playerClient.GetComponent<PlayerControllerB>();
+                DeadBodyInfo body = playerController.deadBody;
+
+                if (body == null)
+                {
+                    return;
+                }
+
+                RaycastHit hit;
+
+                Vector3 position = transform.position + Vector3.up * 6f;
+                if (Physics.Raycast(transform.position, Vector3.up, out hit, 25f, StartOfRound.Instance.collidersAndRoomMaskAndDefault))
+                {
+                    position = hit.point;
+                }
+
+                SetLineRendererPoints component = Instantiate<GameObject>(RopePhysics, position, Quaternion.identity, RoundManager.Instance.mapPropsContainer.transform).GetComponent<SetLineRendererPoints>();
+                component.target.position = body.bodyParts[0].transform.position;
+
+                Instantiate(Noose, body.bodyParts[0].transform, false);
+
+                body.attachedLimb = body.bodyParts[0];
+                body.attachedTo = component.target;
+            }
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            selectedPlayer = nearestPlayer();
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalLib.Modules;
+using UnityEngine;
+using static LethalLib.Modules.Enemies;
+
+namespace TheRollingChair
+{
+    [BepInPlugin(Plugin.GUID, NAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "ccode.chair";
+        public const string NAME = "Rolling Chair";
+        public const string VERSION = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(GUID);
+
+        public static ManualLogSource Log;
+
+        public static EnemyType ChairEnemy;
+
+        public static ConfigEntry<float> RollSpeed;
+
+        private void Awake()
+        {
+            RollSpeed = Config.Bind("General", "Roll Speed", 0.6f, "The movement speed of the chair.");
+
+            Log = Logger;
+            Assets.PopulateAssets();
+
+            ChairEnemy = Assets.MainAssetBundle.LoadAsset<EnemyType>("Chair.asset");
+            var Node = Assets.MainAssetBundle.LoadAsset<TerminalNode>("ChairTN");
+            var Keyword = Assets.MainAssetBundle.LoadAsset<TerminalKeyword>("ChairKW");
+
+            RegisterEnemy(ChairEnemy, 100, LethalLib.Modules.Levels.LevelTypes.All, Node, Keyword);
+
+            NetworkPrefabs.RegisterNetworkPrefab(ChairEnemy.enemyPrefab);
+
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                foreach (var method in methods)
+                {
+                    var attributes = method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
+                    if (attributes.Length > 0)
+                    {
+                        method.Invoke(null, null);
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Assets
+    {
+        public static AssetBundle MainAssetBundle = null;
+        public static void PopulateAssets()
+        {
+            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            MainAssetBundle = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "chairassets"));
+            if (MainAssetBundle == null)
+            {
+                Plugin.Log.LogError("Failed to load custom assets.");
+                return;
+            }
+        }
+    }
+}
